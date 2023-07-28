4a84dee7c5fb37e8607444e1ec4159b0c703d02c
DarPerry <DarJPerry@gmail.com>
2023-07-28 13:53:44 -0500

    Attempt 5

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import { BrowserRouter } from "react-router-dom";
 import MobileApp from "./components/MobileApp/MobileApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <MobileApp />
-        </BrowserRouter>
+        <MobileApp />
     </React.StrictMode>
 );
