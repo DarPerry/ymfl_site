f052ca6d5641b2ab6de0ab5a356cc53d6ccd48fd
DarPerry <DarJPerry@gmail.com>
2023-08-15 00:07:45 -0500

    Adding Payout Rules

--- client/src/pages/RulesPage/RulesPage.jsx
+++ client/src/pages/RulesPage/RulesPage.jsx
@@ -1,9 +1,29 @@
 import Page from "../../components/Page/Page";
 
+import styles from "./RulesPage.module.scss";
+
 const RulesPage = () => {
     return (
         <Page>
-            <div>Rules Page Coming Soon</div>
+            <div className={styles.rule}>
+                <div className={styles.title}>Entry Fee & Payouts</div>
+                <div className={styles.body}>
+                    <ul className={styles.list}>
+                        <li>
+                            Entry Cost: <b>$50</b>
+                        </li>
+                        <li>
+                            Champion: <b>$400</b>
+                        </li>
+                        <li>
+                            Runner-Up: <b>$125</b>
+                        </li>
+                        <li>
+                            Season Points Leader: <b>$75</b>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </Page>
     );
 };
